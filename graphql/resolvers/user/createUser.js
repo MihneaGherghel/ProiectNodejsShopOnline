e3b1a0f6b7a8f1a65cc0dcb3495128fb3348a0b5
narcis.fanica <narcis.fanica@getik.net>
2023-12-23 16:08:48 +0200

    Remove unused code and comments in createUser.js

--- graphql/resolvers/user/createUser.js
+++ graphql/resolvers/user/createUser.js
@@ -1,12 +1,8 @@
 const db = require("../../../models/index");
-const bcrypt = require("bcrypt");
-const checkUser = require("../../../utils/checkUser");
+//const bcrypt = require("bcrypt");
 
 const createUserResolver = async (_, args, context) => {
   const { username, email, password, age, city, address, postal_code } = args;
-
-  const req = context.req;
-  checkUser(req);
 
   //const hash = await bcrypt.hash(password, 10);
 
